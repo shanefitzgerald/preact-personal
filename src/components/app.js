70f42bb29b1159f14d4e7cb18a3e2c5acc7da6c2
shanefitzgerald <sfitzgerald@fandango.com>
2017-10-15 12:48:05 -0700

    added material-ui

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,16 @@
 import { h, Component } from 'preact';
 import { Router } from 'preact-router';
+import React from 'react';
+// import ReactDOM from 'react-dom';
+// import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import Header from './header';
 import Home from '../routes/home';
 import Profile from '../routes/profile';
 import Contact from '../routes/contact';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 // import Home from 'async!./home';
 // import Profile from 'async!./profile';
-console.log(Contact);
 export default class App extends Component {
 
 	/** Gets fired when the route changes.
@@ -18,17 +21,22 @@ export default class App extends Component {
 		this.currentUrl = e.url;
 	};
 
+	// componentDidMount() {
+	// 	injectTapEventPlugin();
+	// }
+
 	render() {
 		return (
-			<div id="app">
-				<Header />
-				<Router onChange={this.handleRoute}>
-					<Home path="/" />
-					<Profile path="/profile/" user="shane" />
-					<Profile path="/profile/:user" />
-					<Contact path="/contact" />
-				</Router>
-			</div>
+			<MuiThemeProvider>
+				<div id="app">
+					<Header />
+					<Router onChange={this.handleRoute}>
+						<Home path="/" />
+						<Profile path="/profile/" user="shane" />
+						<Contact path="/contact" />
+					</Router>
+				</div>
+			</MuiThemeProvider>
 		);
 	}
 }
--- src/routes/contact/index.js
+++ src/routes/contact/index.js
@@ -1,4 +1,8 @@
 import { h, Component } from 'preact';
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+
 import style from './style';
 
 export default class Contact extends Component {
@@ -7,6 +11,7 @@ export default class Contact extends Component {
       <div class={style.contact}>
         <h1>Contact</h1>
         <p>This is the Contact component.</p>
+        <RaisedButton label="Default" />
       </div>
     );
 	}
